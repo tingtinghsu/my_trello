9f68acc2a4886f0ae556351f0626fbd44283ae64
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-23 21:31:43 +0800

    Card Component

--- app/frontend/components/list.vue
+++ app/frontend/components/list.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="list">
     <h2 class="header">{{ list.name }}</h2>
+
+    <div class="cardset">
+      <!-- lists: html @lists.to_json(include: :cards), 所以我們已經有cards可以利用 -->
+      <Card v-for="card in list.cards" :card="card" :key="card.id" ></Card>
+    </div>
   </div>
 </template>
 
 <script>
+  import Card from 'components/card';
   export default {              
     name: 'List',
-    props: ["list"] // property
+    props: ["list"], // property
+    components: { Card }
   }
 </script>
 
@@ -17,6 +24,10 @@
   
   .header {
     @apply .px-3 .py-1 .font-bold;
+  }
+
+  .cardset {
+    @apply .mx-2 .mt-2 .mb-2 .rounded-lg;
   }
 }
 </style>
