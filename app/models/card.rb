9afc43b2a9c09a4f60bea44bc39e82d6b46c298f
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-23 14:14:16 +0800

    card/ list model, acts_as_list

---
+++ app/models/card.rb
@@ -0,0 +1,5 @@
+class Card < ApplicationRecord
+  acts_as_list scope: :list
+  belongs_to :list
+  validates :name, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # 根據position遞增
+  has_many :lists, -> { order(position: :asc) }
 end
