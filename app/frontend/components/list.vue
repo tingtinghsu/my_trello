316cf794099062988c5892d2905d86fdc770bbe5
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-23 21:01:46 +0800

    List Component

---
+++ app/frontend/components/list.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="list">
+    <h2 class="header">{{ list.name }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {              
+    name: 'List',
+    props: ["list"] // property
+  }
+</script>
+
+<style lang="scss" scoped>
+.list {
+  @apply .bg-gray-200 .mx-2 .w-64 .rounded-md;
+  
+  .header {
+    @apply .px-3 .py-1 .font-bold;
+  }
+}
+</style>
--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,18 +1,8 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
 
@@ -21,15 +11,18 @@ import 'styles'
 
 // vue.js
 import Vue from 'vue/dist/vue.esm';
+import List from 'components/list';
+
 document.addEventListener("turbolinks:load", function(event){
-  let el = document.querySelector('#list');
+  let el = document.querySelector('#show-list');
 
   if (el) {
     new Vue({
       el,
       data: {
         lists: JSON.parse(el.dataset.lists)
-      }
+      },
+      components: { List }
     });
   }
 })
